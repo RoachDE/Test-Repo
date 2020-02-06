2093288d9b3f0ae6e8c2359457503f34e2526da7
gipfelgold <gipfelgold@gipfelgldsimac2.speedport_w_921v_1_46_000>
2020-02-06 09:40:51 +0100

    Removed some Code

--- Learning_by_Doing/test_test_test.py
+++ Learning_by_Doing/test_test_test.py
@@ -1,16 +1,4 @@
-import math
-import os
-import sys
-
 import requests
-
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://gipfelgold.de")
